c0f515edf64afe9e3fa324841157a7a74d407180
Muskan <59757988+Muskan-creator@users.noreply.github.com>
2021-01-24 20:51:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let buttons = document.getElementById('buttons');
+let background = document.getElementById('background');
+let title = document.getElementById('title');
+
+function black1() {
+	buttons.src = '../landing page/images/iphone2.png';
+	background.style.backgroundColor = 'black';
+	title.style.color = 'black';
+}
+
+function white1() {
+	buttons.src = '../landing page/images/iphone10.png';
+	background.style.backgroundColor = '#fff';
+	title.style.color = '#fff';
+}
+
+function red1() {
+	buttons.src = '../landing page/images/iphone3.png';
+	background.style.backgroundColor = '#f5111d';
+	title.style.color = '#f5111d';
+}
+
+function blue1() {
+	buttons.src = '../landing page/images/iphone5.png';
+	background.style.backgroundColor = '#bddeda';
+	title.style.color = '#bddeda';
+}
